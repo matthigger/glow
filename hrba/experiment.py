8608166ab1bc3b501223113a0911fa46c217350c
matt higger <matt.higger@gmail.com>
2023-05-26 15:32:08 -0400

    add (incomplete) - Experiment - contains x, y, mask_idx & contrast and methods for building and manipulating an experiment

---
+++ hrba/experiment.py
@@ -0,0 +1,144 @@
+import pathlib
+import re
+from copy import copy
+
+import nibabel as nib
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+class Experiment:
+    """ identifies regions which show some statistically significant effect
+
+    an effect is a linear mapping from x to y within some contiguous set of
+    voxels:
+
+    y = a_0 + a_1 x_1 + a_2 x_2 + ...
+
+    Attributes:
+        x (np.array): (a, num_img) explanatory variables
+        y (np.array): (b, num_img, num_vox) image intensities
+        mask_idx (np.array): same shape as image.  -1 where voxel not
+            included in analysis, otherwise contains voxel index
+        contrast (np.array): (a) True for each corresponding feature in x which
+            is "of interest" (other x features form the reduced model in
+            computing f statistic)
+        x_names  (list): (a) names of each x feature (defaults to None)
+        y_names (list): (b) names of each y feature (defaults to None)
+    """
+
+    def __init__(self, *, x=None, y, mask_idx, contrast=None, x_names=None,
+                 y_names=None):
+        self.x = x
+        self.y = y
+        self.mask_idx = mask_idx
+        self.contrast = contrast
+        self.x_names = x_names
+        self.y_names = y_names
+
+    @classmethod
+    def from_search(cls, folder, sbj_regex, img_glob_dict, **kwargs):
+        """ searches path for matching image files
+
+        Args:
+            folder (str): folder to search (recursively) for image files
+            sbj_regex (str): regex string which matches on subject names,
+                must match once on each image's full path
+            img_glob_dict (dict): keys are str names of each image type
+                (e.g. 'FA', 'MD'), values are glob strs (e.g. '*_FA.nii.gz')
+
+        Returns:
+            experiment (Experiment): experiment generated from all images found
+        """
+        # todo: support passing x as a csv, reconcile with files found and
+        #  error on missing
+
+        # find all images
+        folder = pathlib.Path(folder)
+        df = pd.DataFrame()
+        for y_feat, y_glob in img_glob_dict.items():
+            for file in folder.glob(y_glob):
+                # extract sbj from full path of file
+                sbj_list = re.findall(sbj_regex, str(file))
+                assert len(sbj_list) == 1, \
+                    f'unique sbj not found in file: {file}'
+                sbj = sbj_list[0]
+
+                # todo: check that duplicate file doesn't already exist
+                df.loc[sbj, y_feat] = file
+
+        # check if any subject is missing any imaging feature
+        s_missing = df.isna().mean(axis=1)
+        if s_missing.any():
+            print('some sbj missing files:')
+            print(df.loc[s_missing, :].notnull().astype('int'))
+
+        # build mask_idx, ensure affine & shape are consistent
+        file_first = df.values.flatten()[0]
+        img_first = nib.load(file_first)
+        vox_count = (img_first.get_fdata() != 0).astype(int)
+
+        for file in tqdm(df.values.flatten()[1:],
+                         desc='checking consistent shape'):
+            # check shape
+            img = nib.load(file)
+            assert np.array_equal(img.affine, img_first.affine), \
+                f'affine mismatch: {file}, {img_first}'
+            assert np.array_equal(img.shape, img_first.shape), \
+                f'shape mismatch: {file}, {img_first}'
+
+            # count voxels in image (all nonzero voxels)
+            vox_count += img.get_fdata() != 0
+
+        # build mask_idx
+        mask = vox_count == df.size
+        mask_idx = np.full(mask.shape, fill_value=-1)
+        num_vox = mask.sum()
+        mask_idx[mask] = np.arange(num_vox)
+
+        # load data
+        y = np.empty((len(df.columns), df.shape[0], num_vox))
+        for sbj_idx, sbj in tqdm(enumerate(sorted(df.index)),
+                                 desc='load per sbj'):
+            for feat_idx, y_feat in enumerate(sorted(df.columns)):
+                img = nib.load(df.loc[sbj, y_feat])
+                y[feat_idx, sbj_idx, :] = img.get_fdata()[mask]
+
+        return cls(y=y, y_names=df.columns, mask_idx=mask_idx, **kwargs)
+
+    def bootstrap_y(self, n, seed=None, snr=1):
+        """ returns a new Experiment which bootstrap resamples images
+
+        Args:
+            n (int): number of images in the resulting Experiment
+            seed: used for random number generator
+            snr (float): signal-to-noise ratio, at a snr of 2 the noise std
+                deviation is twice the standard deviation of a y feature (
+                across all voxels and images)
+
+        Returns:
+            exp_out (Experiment): y has been bootstrap resampled from self
+        """
+
+        raise NotImplementedError
+
+    def sample_x(self, a, seed=None):
+        """ generates (or replaces) x with an arbitrary std normal noise
+
+        Args:
+            a (int): number of x features in output
+            seed: used for random number generator
+
+        Returns:
+            exp_out (Experiment): x has been replaced with noise from self
+        """
+        # sample x
+        num_img = self.y.shape[1]
+        rng = np.random.default_rng(seed=seed)
+        x = rng.standard_normal(size=(a, num_img))
+
+        # build a new instance which "copies" self, replacing x
+        d = copy(self.__dict__)
+        d['x'] = x
+        return type(self)(**d)
---
+++ test/test_experiment.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+from itertools import product
+
+import hrba
+from hrba.experiment import *
+
+data_folder = pathlib.Path(hrba.__file__).parents[2] / 'data'
+
+# build some dummy data to load
+folder = pathlib.Path('.') / 'data'
+folder.mkdir(exist_ok=True)
+
+shape = (3, 4, 5)
+affine = np.eye(4)
+img_feat_intensity = defaultdict(dict)
+for intensity, (img_idx, feat_idx) in enumerate(product(range(3),
+                                                        range(2))):
+    # add 1 to intensity (zero values are considered background)
+    intensity += 1
+    file = folder / f'img{img_idx}_feat{feat_idx}.nii.gz'
+    if not file.exists():
+        x = np.full((shape), fill_value=intensity).astype(float)
+        img = nib.Nifti2Image(dataobj=x, affine=affine)
+        img.to_filename(file)
+
+    img_feat_intensity[img_idx][feat_idx] = intensity
+
+
+class TestExperiment:
+    def test_from_search(self):
+        exp = Experiment.from_search(folder=folder,
+                                     sbj_regex='img\d',
+                                     img_glob_dict={'feat0': '*feat0.nii.gz',
+                                                    'feat1': '*feat1.nii.gz'})
+
+        # ensure values arrived at their proper place in y
+        for img_idx, feat_intense in img_feat_intensity.items():
+            for feat_idx, intense in feat_intense.items():
+                assert (exp.y[feat_idx, img_idx, :] == intense).all()
