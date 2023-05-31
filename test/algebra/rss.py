7e219e268bb67698b1f37d593e1c879cc5f6b066
matt higger <matt.higger@gmail.com>
2023-05-31 14:42:34 -0400

    refactor - generate_dummy_data - now its own function in test/helper.py

--- test/algebra/rss.py
+++ test/algebra/rss.py
@@ -1,26 +1,7 @@
 import numpy as np
-
 from numpy.polynomial.polynomial import Polynomial
 
-
-def generate_dummy_data(a=2, b=3, num_img=5, reg_size=10, bias=True,
-                        seed=None, add_effect=False):
-    # generate dummy data
-    rng = np.random.default_rng(seed=seed)
-    x = rng.standard_normal((a, num_img))
-    y = rng.standard_normal((b, num_img, reg_size))
-    contrast = np.ones(a, dtype=bool)
-
-    if bias:
-        contrast[0] = False
-        x[0, :] = 1
-
-    if add_effect:
-        # add strong effect
-        beta = rng.standard_normal((b, a)) * 100
-        y += (beta @ x)[..., np.newaxis]
-
-    return x, y, contrast
+from helper import generate_dummy_data
 
 
 def test_validate_eps():
