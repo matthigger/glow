152e3a6bcb6fa36a29598a23ed65679e91d76272
matt higger <matt.higger@gmail.com>
2023-06-07 13:31:22 -0400

    add - get_perm_matrix()

---
+++ hrba/experiment/permute.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def get_perm_matrix(seed, num_sbj):
+    """ gets (n x n) permutation matrix
+
+    Returns:
+        perm (np.array): (n, n) has exactly one 1 in each row and col
+    """
+    if seed == 0:
+        # by convention, no permutation for seed=0
+        return np.eye(num_sbj)
+
+    rng = np.random.default_rng(seed)
+    return rng.permutation(np.eye(num_sbj))
