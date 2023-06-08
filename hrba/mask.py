c1df5407c540bf7bf4220414f508c39be3e06080
matt higger <matt.higger@gmail.com>
2023-06-08 17:00:51 -0400

    add - mask_from_tree (unfinished) - not sure if its necessary now

--- hrba/mask.py
+++ hrba/mask.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+from hrba.graph import iter_topo
+
+
 def get_mask_idx(mask):
     """ builds mask_idx from mask
 
@@ -15,3 +18,27 @@ def get_mask_idx(mask):
     num_vox = mask.sum()
     mask_idx[mask] = np.arange(num_vox)
     return mask_idx
+
+
+def mask_from_tree(mask_idx, dendro, reg_idx):
+    """ builds mask of single region from dendrogram (slow)
+
+    Args:
+        mask_idx (np.array): -1 where voxels not to be analyzed, all other
+            voxels get a unique integer (voxel index)
+        dendro (np.array): (num_leaf - 1, 2) dendrogram arrays (equiv to
+            sklearn.cluster.Ward.children_)
+        reg_idx (int): region index
+
+    Returns:
+        mask (np.array): False where region doesn't contain voxel,
+            True otherwise
+    """
+    mask = np.zeros(mask_idx.shape, dtype=bool)
+    num_leaf = dendro.shape[0] + 1
+
+    for _reg_idx in iter_topo(dendro, node_start=reg_idx):
+        if _reg_idx < num_leaf:
+            mask[mask_idx == _reg_idx] = True
+
+    return mask
