a36c72a03012c464444face966836624c75e7722
matt higger <matt.higger@gmail.com>
2023-06-08 11:47:51 -0400

    add - graph.get_hit_miss() - counts hit / miss voxels towards some target over whole tree

--- hrba/graph.py
+++ hrba/graph.py
@@ -1,8 +1,11 @@
-def node_sum(dend, val_dict):
+import numpy as np
+
+
+def node_sum(dendro, val_dict):
     """ given item per leaf in tree, sums leaf values and adds to dictionary
 
     Args:
-        dend (np.array): (2, num_leaf) dendrogram arrays (equiv to
+        dendro (np.array): (2, num_leaf) dendrogram arrays (equiv to
             sklearn.cluster.Ward.children_)
         val_dict (dict): keys are leaf indices (0 to num_leaf), values
             are items to be summed
@@ -12,9 +15,32 @@ def node_sum(dend, val_dict):
             nodes, not just leafs
     """
     num_leaf = len(val_dict)
-    for node_idx, (c0, c1) in enumerate(dend.T):
+    for node_idx, (c0, c1) in enumerate(dendro.T):
         node_idx += num_leaf
         new_val = val_dict[c0] + val_dict[c1]
         val_dict[node_idx] = new_val
 
     return val_dict
+
+
+def get_hit_miss(mask, mask_idx, dendro):
+    """ for each node, count how many voxels are in / out of effect
+
+    Args:
+        mask (np.array): target mask (boolean, same shape as mask_idx)
+        mask_idx (np.array):
+        dendro (np.array): dendrogram
+
+    Returns:
+        miss_hit_dict (dict): keys are node indices, values are (2) arrays
+            containing number of voxels misses (effect voxels not in
+            region) and hits (effect voxels in region)
+    """
+    # build miss_hit_dict for leaf nodes
+    num_vox = (mask_idx >= 0).sum()
+    hit = np.zeros(num_vox)
+    hit[mask_idx[mask.astype(bool)]] = 1
+    miss = np.ones(num_vox) - hit
+    miss_hit_dict = dict(enumerate(np.vstack((miss, hit)).T))
+
+    return node_sum(dendro=dendro, val_dict=miss_hit_dict)
--- test/test_graph.py
+++ test/test_graph.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from hrba.graph import *
 
 
@@ -11,3 +9,23 @@ def test_iter_node_sum():
     # each value is the sum of its children's values
     val_dict_exp = {0: 0, 1: 1, 2: 2, 3: 3, 4: 1, 5: 5, 6: 6}
     assert node_sum(dend, val_dict) == val_dict_exp
+
+
+def test_get_hit_miss():
+    mask = np.array([0, 0, 1, 1])
+    mask_idx = np.arange(4)
+    dendro = np.array([[0, 2, 4],
+                       [1, 3, 5]])
+    miss_hit_dict_exp = {0: np.array([1, 0]),
+                         1: np.array([1, 0]),
+                         2: np.array([0, 1]),
+                         3: np.array([0, 1]),
+                         4: np.array([2, 0]),
+                         5: np.array([0, 2]),
+                         6: np.array([2, 2])}
+    miss_hit_dict = get_hit_miss(mask=mask, mask_idx=mask_idx, dendro=dendro)
+
+    assert miss_hit_dict.keys() == miss_hit_dict_exp.keys()
+    for node in miss_hit_dict.keys():
+        assert np.allclose(miss_hit_dict[node],
+                           miss_hit_dict_exp[node]), f'failure node: {node}'
