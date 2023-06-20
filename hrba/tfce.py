36170fde568611e153142fae704cdccb2e320fc6
matt higger <matt.higger@gmail.com>
2023-06-20 12:43:24 -0400

    add - tfce.apply_tfce() - currently only applies tfce to 3d images

---
+++ hrba/tfce.py
@@ -0,0 +1,45 @@
+import os
+import pathlib
+import subprocess
+import tempfile
+import warnings
+from shutil import which
+
+import nibabel as nib
+import numpy as np
+
+fsl_path = pathlib.Path('/usr/local/fsl')
+fsl_conf_sh = fsl_path / 'etc/fslconf/fsl.sh'
+fslmaths_path = fsl_path / 'bin/fslmaths'
+
+if not which(str(fslmaths_path)):
+    warnings.warn('fslmaths not found')
+
+
+def apply_tfce(x):
+    """ applies tfce to an array (must be 3d)
+
+    Args:
+        x (np.array): a 3d array to apply tfce to
+    """
+
+    # get input / output files
+    f = tempfile.NamedTemporaryFile(suffix='.nii.gz').name
+    f_x_in = f.replace('.nii', '_x.nii')
+    f_out = f.replace('.nii', '_x_tfce.nii')
+
+    # write input to disk
+    img = nib.Nifti1Image(x, affine=np.eye(4))
+    img.to_filename(f_x_in)
+
+    cmd = f'source {fsl_conf_sh} && {fslmaths_path} {f_x_in} -tfce 2 .5 6 {f_out}'
+    proc = subprocess.run(cmd, shell=True, capture_output=True,
+                          executable='/bin/bash')
+
+    x_tfce = nib.load(f_out).get_fdata()
+
+    # cleanup
+    os.remove(f_out)
+    os.remove(f_x_in)
+
+    return x_tfce
