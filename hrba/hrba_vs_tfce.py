929869ed4a40e841c1e57e2c685e9d84727ba1cb
matt higger <matt.higger@gmail.com>
2023-07-14 15:44:08 -0400

    revise - hrba_vs_tfce.py - now parallel (thank you joblib!)
    (cherry picked from commit 9aa4e416a358238d1c20286799ca052403b5c667)

--- hrba/hrba_vs_tfce.py
+++ hrba/hrba_vs_tfce.py
@@ -1,14 +1,15 @@
 import shutil
 from datetime import datetime
+from uuid import uuid4
 
 import cloudpickle
-from tqdm import tqdm
+from joblib import Parallel, delayed
 
 from hrba.experiment import *
 from hrba.sample_effect import *
 
 # build experiment with strong effect to be found (whole region)
-p_val_all = np.geomspace(.1, .01, 11)
+p_val_all = np.geomspace(.15, .03, 15)
 effect_perc = .2
 n_permute = 100
 alpha = .05
@@ -37,36 +38,32 @@ exp_hcp.sample_x(a=2)
 
 analysis_obj_tup = (AnalysisTFCE, AnalysisHRBA)
 
-ana_idx_max = n_repeat * p_val_all.size * len(analysis_obj_tup)
-z_width = int(np.ceil(np.log10(ana_idx_max)))
-ana_idx = 0
-with tqdm(desc='experiment (pval-seed-method pair)',
-          total=p_val_all.size * n_repeat * len(analysis_obj_tup)) as pbar:
-    for seed in range(n_repeat):
-        # trim experiment to reasonable size (for speedup)
-        extenter = ExtenterSphere(radius=radius)
-        mask_all = extenter(mask_idx=exp_hcp.mask_idx, seed=seed)
-        exp = exp_hcp.apply_mask(mask_all)
 
-        # sample effect space
-        extenter = ExtenterMinVar(n=exp.y.shape[2] * effect_perc)
-        mask_target = extenter(y=exp.y, mask_idx=exp.mask_idx, seed=seed)
+def run_one_exp(seed):
+    # trim experiment to reasonable size (for speedup)
+    extenter = ExtenterSphere(radius=radius)
+    mask_all = extenter(mask_idx=exp_hcp.mask_idx, seed=seed)
+    exp = exp_hcp.apply_mask(mask_all)
 
-        for p_val in p_val_all:
-            # impose effect
-            _exp, effect = exp.impose_effect(seed=seed, mask=mask_target,
-                                             p_val=p_val)
+    # sample effect space
+    extenter = ExtenterMinVar(n=exp.y.shape[2] * effect_perc)
+    mask_target = extenter(y=exp.y, mask_idx=exp.mask_idx, seed=seed)
 
-            for Ana in analysis_obj_tup:
-                ana = Ana(exp=_exp, alpha=alpha, n_permute=n_permute)
-                ana.run(verbose=False)
+    for p_val in p_val_all:
+        # impose effect
+        _exp, effect = exp.impose_effect(seed=seed, mask=mask_target,
+                                         p_val=p_val)
 
-                # dump
-                s_ana_idx = str(ana_idx).zfill(z_width)
-                file_out = folder_out / f'analysis{s_ana_idx}.p'
-                with open(file_out, 'wb') as file:
-                    cloudpickle.dump((p_val, seed, Ana.__name__, ana, effect),
-                                     file=file)
+        for Ana in analysis_obj_tup:
+            ana = Ana(exp=_exp, alpha=alpha, n_permute=n_permute)
+            ana.run(verbose=False)
 
-                ana_idx += 1
-                pbar.update(1)
+            # dump
+            file_out = folder_out / f'analysis_{str(uuid4())[:8]}.p'
+            with open(file_out, 'wb') as file:
+                cloudpickle.dump((p_val, seed, Ana.__name__, ana, effect),
+                                 file=file)
+
+
+r = Parallel(n_jobs=-2, verbose=10)(
+    delayed(run_one_exp)(seed) for seed in range(n_repeat))
