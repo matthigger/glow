6ab0ee029717c6f8fb0dc98c296bf692e670113c
higger <matt.higger@gmail.com>
2024-02-05 15:26:51 -0500

    revise - hrba.plot

--- hrba/plot.py
+++ hrba/plot.py
@@ -8,49 +8,62 @@ from hrba.graph import get_f1
 
 sns.set(font_scale=1.3)
 
-
-def scatter_summary(*, epoch, **kwargs):
-    fig, ax = plt.subplots(3, 1)
-
-    plt.sca(ax[0])
-    ax[0].title.set_text('modelling LLR vs size (H0)')
-    scatter_size_vs_stat(epoch=epoch, y_feat=np.log(epoch.llr[1:, :]),
-                         size=np.log(epoch.size[1:, :]))
-    size = epoch.size[1:, :].flatten()
-    size = np.linspace(size.min(), size.max(), 101)
-    mu, var = epoch.llr_model.predict(size)
-    plt.plot(np.log(size), mu, color='b', label='mean')
-    plt.fill_between(np.log(size), mu + var ** .5, mu - var ** .5,
-                     color='b', alpha=.3, label='+/- 1 std')
-    plt.legend()
-    plt.xscale('linear')
-    plt.yscale('linear')
-    plt.xlabel('log_e size')
-    plt.ylabel('log_e llr')
-
-    plt.sca(ax[1])
-    ax[1].title.set_text('Region Stat Significance')
-    scatter_size_vs_stat(epoch=epoch, y_feat=epoch.z_stat, **kwargs, )
-    thresh = np.percentile(epoch.z_stat.max(axis=1), [95])
-    plt.axhline(thresh, color='r', linestyle='--')
-    plt.ylabel('z-stat')
-    plt.yscale('linear')
-
-    plt.sca(ax[2])
-    ax[2].title.set_text('Region Discovery')
-    scatter_size_vs_stat(epoch=epoch, y_feat=epoch.llr, **kwargs, )
-    plt.ylabel('llr')
-
-    return fig
-
-
-def scatter_size_vs_stat(epoch, y_feat, size=None, mask=None, min_size=1):
+def scatter_size_vs_stat_plotly(epoch, y_feat, size=None, mask=None, min_size=1):
     """ scatters size vs f_stat, colors by f1 score if mask is passed
 
      Args:
         epoch (Epoch):
         y_feat (np.array): y feature to plot (same size as size)
         size (np.array): size of each region to plot (defaults to epoch.size)
+        mask (np.array): target mask
+        min_size (int): smallest size to be plotted
+     """
+    # compute f1 score
+    if mask is not None:
+        f1 = get_f1(mask=mask,
+                    mask_idx=epoch.exp.mask_idx,
+                    children=epoch.child_dict[0])
+    else:
+        f1 = None
+
+    if size is None:
+        size = epoch.size.astype(float)
+
+    assert isinstance(y_feat, np.ndarray)
+    assert y_feat.shape == size.shape
+    y = copy(y_feat)
+
+    if min_size > 1:
+        b = epoch.size < min_size
+        size[b] = np.nan
+        y[b] = np.nan
+
+    if mask is None:
+        plt.scatter(size, y, alpha=.02, color='k',
+                    linewidth=0, label='region')
+    else:
+        plt.scatter(size[1:, :], y[1:, :], alpha=.02, color='k',
+                    linewidth=0, label='region (permuted)')
+
+        b = f1 > 0
+        plt.scatter(size[0, :][b], y[0, :][b], c=f1.flatten()[b],
+                    cmap='plasma', label='region w/ target', marker='s')
+        cbar = plt.colorbar()
+        cbar.set_label('F1 score', rotation=90)
+
+    plt.xlabel('size')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.legend()
+
+
+
+def scatter_size_vs_stat(epoch, y_feat, mask=None, min_size=1):
+    """ scatters size vs f_stat, colors by f1 score if mask is passed
+
+     Args:
+        epoch (Epoch):
+        y_feat (np.array): y feature to plot (same size as size)
         mask (np.array): target mask
         min_size (int): smallest size to be plotted
      """
@@ -63,8 +76,7 @@ def scatter_size_vs_stat(epoch, y_feat, size=None, mask=None, min_size=1):
     else:
         f1 = None
 
-    if size is None:
-        size = epoch.size.astype(float)
+    size = epoch.size.astype(float)
 
     assert isinstance(y_feat, np.ndarray)
     assert y_feat.shape == size.shape
