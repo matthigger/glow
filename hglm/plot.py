a35b1de98da6234a39bb2761ada1ab40a3f82b5c
higger <matt.higger@gmail.com>
2024-03-08 15:42:10 -0500

    delete - plot.scatter_size_vs_stat_plotly() - never actually used plotly, redundant (this shouldn't have snuck into code, oops!)

--- hglm/plot.py
+++ hglm/plot.py
@@ -124,57 +124,6 @@ def image_iter(children, mask_idx, num_vox):
             yield image, mask_idx_current, color_dict
 
 
-def scatter_size_vs_stat_plotly(analysis, y_feat, size=None, mask=None,
-                                min_size=1):
-    """ scatters size vs f_stat, colors by f1 score if mask is passed
-
-     Args:
-        analysis (Analysis):
-        y_feat (np.array): y feature to plot (same size as size)
-        size (np.array): size of each region to plot (defaults to
-            analysis.size)
-        mask (np.array): target mask
-        min_size (int): smallest size to be plotted
-     """
-    # compute f1 score
-    if mask is not None:
-        f1 = get_f1(mask=mask,
-                    mask_idx=analysis.exp.mask_idx,
-                    children=analysis.child_dict[0])
-    else:
-        f1 = None
-
-    if size is None:
-        size = analysis.size.astype(float)
-
-    assert isinstance(y_feat, np.ndarray)
-    assert y_feat.shape == size.shape
-    y = copy(y_feat)
-
-    if min_size > 1:
-        b = analysis.size < min_size
-        size[b] = np.nan
-        y[b] = np.nan
-
-    if mask is None:
-        plt.scatter(size, y, alpha=.02, color='k',
-                    linewidth=0, label='region')
-    else:
-        plt.scatter(size[1:, :], y[1:, :], alpha=.02, color='k',
-                    linewidth=0, label='region (permuted)')
-
-        b = f1 > 0
-        plt.scatter(size[0, :][b], y[0, :][b], c=f1.flatten()[b],
-                    cmap='plasma', label='region w/ target', marker='s')
-        cbar = plt.colorbar()
-        cbar.set_label('F1 score', rotation=90)
-
-    plt.xlabel('size')
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.legend()
-
-
 def scatter_size_vs_stat(analysis, y_feat, mask=None, min_size=1):
     """ scatters size vs f_stat, colors by f1 score if mask is passed
 
