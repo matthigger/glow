d4a2bada9f38cf5262cbefb98e3788a0995fed2f
higger <matt.higger@gmail.com>
2024-04-22 15:32:58 -0400

    revise - AnalysisHGLM - now uses z scores (permute per every permutation-region pair) instead of the log llr = log_size * m + b model to adjust llr.  (nested permutations ... complex for computer and my own brain to think about)

--- hglm/experiment/analysis.py
+++ hglm/experiment/analysis.py
@@ -12,6 +12,7 @@ from hglm.effect import Effect
 from hglm.graph import iter_topo, node_sum, iter_size_yout_ybar
 from hglm.tfce import apply_tfce_x
 from .exper import ExperimentWhitened
+from .permute import Permuter
 from .regress import get_llr, ComputeRegress
 
 
@@ -50,7 +51,7 @@ class Analysis:
             assert not np.all(mask_exclude, axis=1).any(), \
                 'may not exclude an entire permutation'
 
-            # set all excluded stats to the minimum observed
+            # set all excluded stats to nan
             stat = copy(stat).astype(float)
             stat[mask_exclude] = np.nan
 
@@ -70,31 +71,46 @@ class Analysis:
         return pval
 
     @classmethod
-    def get_llr(cls, exp, children=None):
+    def get_llr(cls, exp, n_perm, children=None):
         """ computes log likelihood score (full over reduced) per region
 
         Args:
             exp (Experiment):
+            n_perm (int): number of permutations (includes unpermuted data)
             children (np.array): (num_reg, 2) each col are index of child
                 regions, if none passed then iterates only through voxels
 
         Returns:
-            llr (np.array): (num_reg, ) log likelihood
+            llr (np.array): (num_reg, n_perm) log likelihood
         """
         # compute llr per region
         b, num_img, num_reg = exp.y.shape
         if children is not None:
             num_reg += children.shape[0]
-        llr = np.zeros(num_reg)
+        llr = np.zeros((n_perm, num_reg))
 
         # prepare the get_eps functions (eps0 is only features not-of-interest)
-        comp_reg0 = ComputeRegress(exp.x[~exp.contrast, :])
+        x0 = exp.x[~exp.contrast, :]
+        comp_reg0 = ComputeRegress(x0)
         comp_reg1 = ComputeRegress(exp.x)
 
-        for reg_idx, size, yout, ybar in iter_size_yout_ybar(exp.y, children):
-            eps0 = comp_reg0.get_eps(size, yout, ybar)
-            eps1 = comp_reg1.get_eps(size, yout, ybar)
-            llr[reg_idx] = get_llr(size=size, eps0=eps0, eps1=eps1)
+        # prep Permuter object (if needed)
+        perm = None if n_perm is None else Permuter(x=x0)
+        for reg_idx, size, yout, ybar in iter_size_yout_ybar(exp.y, children,
+                                                             perm=perm,
+                                                             n_perm=n_perm,
+                                                             keep_orig=True):
+            for perm_idx in range(n_perm):
+                # compute llr & store
+                eps0 = comp_reg0.get_eps(size,
+                                         yout[..., perm_idx],
+                                         ybar[..., perm_idx])
+                eps1 = comp_reg1.get_eps(size,
+                                         yout[..., perm_idx],
+                                         ybar[..., perm_idx])
+                llr[perm_idx, reg_idx] = get_llr(size=size,
+                                                 eps0=eps0,
+                                                 eps1=eps1)
 
         return llr
 
@@ -105,13 +121,7 @@ class AnalysisTFCE(Analysis):
 
         # compute llr per each voxel (for every permutation)
         num_vox = exp.y.shape[2]
-        self.llr = np.zeros((n_perm + 1, num_vox))
-        tqdm_dict = dict(total=n_perm + 1,
-                         desc='permuting',
-                         disable=not verbose)
-        for perm_idx in tqdm(range(n_perm + 1), **tqdm_dict):
-            _exp = exp.permute(perm_idx, block_exchange=False)
-            self.llr[perm_idx, :] = self.get_llr(_exp, children=None)
+        self.llr = self.get_llr(exp, n_perm=n_perm + 1, children=None)
 
         # apply TFCE per image
         self.tfce_stat = self.apply_tfce(stat=self.llr,
@@ -179,8 +189,8 @@ class AnalysisHGLM(Analysis):
     Attributes:
         child_dict (dict): keys are permutation indices, values are
             (2, n) graph arrays (equiv to sklearn.cluster.Ward.children_)
-        llr (np.array): (n_perm + n_perm_adj + 1, num_reg)
-        size (np.array): (n_perm + n_perm_adj + 1, num_reg) number of voxels in
+        llr (np.array): (n_perm + 1, n_perm_adj, num_reg)
+        size (np.array): (n_perm + 1, num_reg) number of voxels in
             each region (for all permutations).  first row corresponds to
             unpermuted data
     """
@@ -194,8 +204,7 @@ class AnalysisHGLM(Analysis):
 
         # permute, cluster & llr per region in hierarchy
         self.child_dict = dict()
-        self.llr = np.zeros((n_perm + n_perm_adj + 1, num_reg))
-        tqdm_dict = dict(total=n_perm + n_perm_adj + 1,
+        tqdm_dict = dict(total=n_perm + 1,
                          desc='permuting',
                          disable=not verbose)
 
@@ -207,78 +216,50 @@ class AnalysisHGLM(Analysis):
             children = self.cluster(exp=_exp)
 
             # compute log likelihood ratio
-            llr = self.get_llr(_exp, children)
+            llr = self.get_llr(exp=_exp, children=children, n_perm=n_perm_adj)
             return children, llr
 
-        perm_iter = tqdm(range(n_perm + n_perm_adj + 1), **tqdm_dict)
+        perm_iter = tqdm(range(n_perm + 1), **tqdm_dict)
         if n_jobs not in (0, 1):
             # parallel
             r = Parallel(n_jobs=n_jobs)(delayed(cluster_llr)(perm)
                                         for perm in perm_iter)
 
             # store
+            llr_list = list()
             for p_idx, (child, llr) in enumerate(r):
                 self.child_dict[p_idx] = child
-                self.llr[p_idx, :] = llr
+                llr_list.append(llr)
+            self.llr = np.stack(llr_list, axis=0)
         else:
             # serial
+            self.llr = np.zeros((n_perm + 1, n_perm_adj, num_reg))
             for p_idx in perm_iter:
-                self.child_dict[p_idx], self.llr[p_idx, :] = cluster_llr(p_idx)
+                child, llr = cluster_llr(p_idx)
+                self.child_dict[p_idx] = child
+                self.llr[p_idx, :, :] = llr
 
         # compute sizes of each region
-        self.size = np.empty((n_perm + n_perm_adj + 1, num_reg))
+        self.size = np.empty((n_perm + 1, num_reg))
         for perm_idx, children in self.child_dict.items():
             self.size[perm_idx, :] = node_sum(x=np.ones(num_vox, dtype=int),
                                               children=children)
 
         # adjust llr
-        self.llr_beta = self.llr_fit(self.size[-n_perm_adj:, :],
-                                     self.llr[-n_perm_adj:, :])
-        llr_predict = self.llr_predict(self.llr_beta,
-                                       self.size[:-n_perm_adj, :])
-        self.llr_adjust = 10 ** (np.log10(self.llr[:-n_perm_adj, :]) -
-                                 np.log10(llr_predict))
+        mu = self.llr[:, 1:, :].mean(axis=1)
+        std = self.llr[:, 1:, :].std(axis=1)
+        self.z = (self.llr[:, 0, :] - mu) / std
 
         # compute p-values (max stat across space)
-        mask_exclude = self.size[:-n_perm_adj, :] < min_size_discover
-        self.p_val = self.get_pval(stat=self.llr_adjust,
+        mask_exclude = self.size < min_size_discover
+        self.p_val = self.get_pval(stat=self.z,
                                    mask_exclude=mask_exclude)
 
         # discover effects (greedily choose max z stat regions whose p_val is
         # significant.  continue so long as disjoint significant effect remain)
         self.effect_list = self.discover(pval=self.p_val, alpha=alpha,
-                                         stat=self.llr_adjust[0, :], exp=exp,
+                                         stat=self.z[0, :], exp=exp,
                                          children=self.child_dict[0])
-
-    @classmethod
-    def llr_fit(cls, size, llr):
-        """ build a model between size and llr
-
-        log size * beta[1] + beta[0] = log llr
-
-        Args:
-            size (np.array): size of each region
-            llr (np.array): log likelihood ratio of each region
-
-        Returns:
-            beta (np.array): (2) model params
-        """
-        x_size = np.vstack([np.ones(size.size), np.log10(size.flatten())])
-        y_llr = np.log10(llr.flatten())
-        return y_llr @ np.linalg.pinv(x_size)
-
-    @classmethod
-    def llr_predict(cls, beta, size):
-        """ predicts llr via model.  see llr_fit()
-
-        Args:
-            beta (np.array): (2) model params
-            size (np.array): size of each region
-
-        Returns:
-            beta (np.array): (2) model params
-        """
-        return 10 ** (beta[0] + beta[1] * np.log10(size))
 
     @classmethod
     def cluster(cls, exp, mode='full'):
