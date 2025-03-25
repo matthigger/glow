da0662eb67b090efb6b909d7c877b17d1e86154d
matt higger <matt.higger@gmail.com>
2025-03-25 12:47:32 -0400

    revise - impose.compute_offset() - now works with chi2 approximation of wilk's lambda (rather than previous "multivariate f stat").  this temporarily breaks backwards compatibility, need to develop interface / functionality to support a roughness coefficient

--- hglm/effect/impose.py
+++ hglm/effect/impose.py
@@ -1,12 +1,27 @@
 import numpy as np
+import scipy.stats
 from scipy.optimize import minimize
-from scipy.stats import f
 
 import hglm.experiment
 import hglm.f_stat
 
 
-def compute_offset(x, y, contrast, f_stat=None, p_val=None, rough=None):
+def wilks_to_chi2(wilks, a, b, n):
+    df = a * b
+    chi2 = -(n - 0.5 * (a + b + 1)) * np.log(wilks)
+    return chi2, df
+
+
+def chi2_to_wilks(chi2, a, b, n):
+    scale = -(n - 0.5 * (a + b + 1))
+    return np.exp(chi2 / scale)
+
+
+def get_wilks(e, h):
+    return np.linalg.det(e) / np.linalg.det(e + h)
+
+
+def compute_offset(x, y, contrast, pval=None, rough=None):
     """ get offset to y, constant across voxels, which imposes an f-stat
 
     Args:
@@ -15,8 +30,7 @@ def compute_offset(x, y, contrast, f_stat=None, p_val=None, rough=None):
         contrast (np.array): (a) True for each corresponding feature in x which
             is "of interest" (other x features form the reduced model in
             computing f statistic)
-        f_stat (float): target f statistic
-        p_val (float): p-value may be passed in place of f stat
+        pval (float): p-value may be passed in place of f stat
         rough (float): roughness coefficient desired.  a ratio of the spatial
             covariance over the power of error in the reduced model
 
@@ -32,51 +46,42 @@ def compute_offset(x, y, contrast, f_stat=None, p_val=None, rough=None):
             roughness coefficient, None if "rough" is not input.
         rough (float): roughness coefficient achieved
     """
-    assert (f_stat is None) != (p_val is None), 'f_stat xor p_val required'
-
     # prep constants
-    a = (~contrast).sum(), contrast.size
+    a0 = (~contrast).sum()
+    a1 = contrast.size - a0
     b, num_img, reg_size = y.shape
 
     # prep matrices
     y_mean = y.mean(axis=2)
 
-    if f_stat is None:
-        # get target f_stat to impose given p_value
-        dfn, dfd = hglm.f_stat.get_f_degrees(num_img, reg_size, a)
-        f_stat = f.ppf(1 - p_val, dfn=dfn, dfd=dfd)
-
-    # collect constants into k
-    k = f_stat / hglm.f_stat.get_f_const(y, contrast)
+    # get target chi2 to impose given p_value
+    df = a1 * b
+    chi2_target = scipy.stats.chi2.ppf(1 - pval, df=df)
 
     # qr decomposition of x
     q = hglm.experiment.decompose(x, contrast)
-    q1_norm2 = np.linalg.norm(q[1] @ y_mean.T) ** 2
-    q2_norm2 = np.linalg.norm(q[2] @ y_mean.T) ** 2
+    yq1q1y = y_mean @ q[1].T @ q[1] @ y_mean.T
+    yq2q2y = y_mean @ q[2].T @ q[2] @ y_mean.T
+    yq1_norm2 = np.linalg.norm(y_mean @ q[1].T) ** 2
+    yq2_norm2 = np.linalg.norm(y_mean @ q[2].T) ** 2
 
     # compute norm of spatial cov square of t
     # |r| \Sigma_r & = \sum_{v \in r} (Y_v - \bar{Y}_r)(Y_v - \bar{Y}_r)^T
     #              & = Y_r Y_r^T - |r| \bar{Y}_r \bar{Y}_r^T
     yr = y.reshape((y.shape[0], -1), order='F')
     sigma = yr @ yr.T / reg_size - y_mean @ y_mean.T
-    sigma_tr = np.trace(sigma) / num_img
 
-    if rough is None:
-        def constraint(alpha):
-            """ when this function output is zero, F-stat is achieved """
-            a1, a2 = alpha
-            return k * sigma_tr * num_img + \
-                k * (1 + a2) ** 2 * q2_norm2 - \
-                (1 + a1) ** 2 * q1_norm2
+    if rough is not None:
+        raise NotImplementedError
 
-    else:
-        assert rough >= 0
-
-        def constraint(alpha):
-            """ when this function output is zero, F-stat is achieved """
-            a1, a2 = alpha
-            return k * (1 + rough) * (1 + a2) ** 2 * q2_norm2 - \
-                (1 + a1) ** 2 * q1_norm2
+    def constraint(alpha):
+        """ when this function output is zero, chi2_target achieved """
+        alpha1, alpha2 = alpha
+        h = (1 + alpha1) ** 2 / num_img * yq1q1y
+        e = sigma + (1 + alpha2) ** 2 / num_img * yq2q2y
+        wilks = get_wilks(e, h)
+        chi2, _ = wilks_to_chi2(wilks, a=a1, b=b, n=num_img)
+        return (chi2 - chi2_target) ** 2
 
     def obj(alpha):
         r"""  ||\Delta||^2 = \sum_{i=1}^3 \alpha_i^2 ||Q_i \bar{Y}_r^T||^2
@@ -85,9 +90,9 @@ def compute_offset(x, y, contrast, f_stat=None, p_val=None, rough=None):
              alpha (tuple): a1, a2 (per equations)
         """
         a1, a2 = alpha
-        return a1 ** 2 * q1_norm2 + a2 ** 2 * q2_norm2
+        return a1 ** 2 * yq1_norm2 + a2 ** 2 * yq2_norm2
 
-    # find scale of each offset which achieves F while being as close as
+    # find scale of each offset which achieves chi2 while being as close as
     # possible to original problem
     res = minimize(fun=obj, x0=np.array([0, 0]),
                    constraints=[{'type': 'eq',
@@ -95,19 +100,9 @@ def compute_offset(x, y, contrast, f_stat=None, p_val=None, rough=None):
                    options={'maxiter': 1000})
     assert res.success, 'optimization failed'
 
-    a1, a2 = res.x
-    offset = a1 * y_mean @ q[1].T @ q[1] + a2 * y_mean @ q[2].T @ q[2]
+    # compute offset
+    alpha1, alpha2 = res.x
+    offset = alpha1 * y_mean @ q[1].T @ q[1] + \
+             alpha2 * y_mean @ q[2].T @ q[2]
 
-    if rough is None:
-        # compute roughness achieved (None was passed)
-        rough = sigma_tr * num_img / ((1 + a2) ** 2 * q2_norm2)
-
-        # no spatial covariance scaling needed
-        sigma_gain = None
-
-    else:
-        # compute spatial covariance scaling assumed in computation above (to
-        # achieve given roughness)
-        sigma_gain = rough * (1 + a2) ** 2 * q2_norm2 / (sigma_tr * num_img)
-
-    return offset, sigma_gain, rough
+    return offset
--- test/effect/test_offset.py
+++ test/effect/test_offset.py
@@ -1,38 +1,52 @@
 from hglm.effect.impose import *
-from hglm.experiment import scale_sigma, get_rough, get_sigma_from_y
 from hglm.f_stat import *
 from ..helper import generate_dummy_data
 
 
 def test_compute_offset():
-    # range(100) takes ~35 seconds to run
     for seed in range(1):
         # generate dummy data
         x, y, contrast = generate_dummy_data(seed=seed)
+        b, num_img, num_vox = y.shape
 
-        for f_stat_exp in (0, 1, 10, 100, 1e6):
-            for rough_exp in (None, 0, 1, 10):
+        for pval_exp in np.logspace(0, -3, 5):
+            for rough_exp in (None,):
                 # compute offset and space_cov_scale needed to achieve f stat
-                offset, sigma_gain, rough = compute_offset(x=x, y=y,
-                                                           contrast=contrast,
-                                                           f_stat=f_stat_exp,
-                                                           rough=rough_exp)
+                offset = compute_offset(x=x, y=y,
+                                        contrast=contrast,
+                                        pval=pval_exp,
+                                        rough=rough_exp)
 
-                # impose effect that f stat is achieved
+                # impose effect that chi2 stat is achieved
                 _y = y + offset[..., np.newaxis]
-                assert (sigma_gain is None) == (rough_exp is None), \
-                    'sigma_gain only when we pass desired roughness'
-                if sigma_gain is not None:
-                    _y = scale_sigma(y=_y, gain=sigma_gain)
+                # assert (sigma_gain is None) == (rough_exp is None), \
+                #     'sigma_gain only when we pass desired roughness'
+                # if sigma_gain is not None:
+                #     _y = scale_sigma(y=_y, gain=sigma_gain)
 
-                # confirm proper f stat achieved
-                f_stat_obs = get_f_stat(x=x, y=_y, contrast=contrast)
-                assert np.isclose(f_stat_obs, f_stat_exp)
+                # compute sigma
+                y_mean = _y.mean(axis=2)
+                yr = _y.reshape((_y.shape[0], -1), order='F')
+                sigma = yr @ yr.T / num_vox - y_mean @ y_mean.T
 
-                if rough_exp is not None:
-                    # ensure desired roughness is achieved
-                    assert np.isclose(rough_exp, rough)
+                # compute observed e and h
+                q = hglm.experiment.decompose(x, contrast)
+                yq1q1y = y_mean @ q[1].T @ q[1] @ y_mean.T
+                yq2q2y = y_mean @ q[2].T @ q[2] @ y_mean.T
+                h = yq1q1y / num_img
+                e = sigma + yq2q2y / num_img
 
-                # check that roughness is computed properly
-                _rough_exp = get_rough(x, _y)
-                assert np.isclose(_rough_exp, rough)
+                # ensure proper pval achieved
+                wilks = get_wilks(e, h)
+                chi2, df = wilks_to_chi2(wilks, a=contrast.sum(), b=b,
+                                        n=num_img)
+                pval = 1 - scipy.stats.chi2.cdf(chi2, df=df)
+                assert np.isclose(pval, pval_exp)
+
+                # if rough_exp is not None:
+                #     # ensure desired roughness is achieved
+                #     assert np.isclose(rough_exp, rough)
+                #
+                # # check that roughness is computed properly
+                # _rough_exp = get_rough(x, _y)
+                # assert np.isclose(_rough_exp, rough)
