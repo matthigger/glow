8b56bca0bbc9a855eb69b81e59a6130cd9f64faf
matt higger <matt.higger@gmail.com>
2025-06-26 12:49:19 -0400

    add - regress.get_rough() - new rough definition: ratio of sigma within region to a comparison region (rest of volume or a shell around the region, depending on inputs)

--- hglm/experiment/regress.py
+++ hglm/experiment/regress.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.ndimage import binary_dilation
+
 
 def get_manova(x, y, contrast):
     b, num_img, num_vox = y.shape
@@ -13,7 +15,7 @@ def get_manova(x, y, contrast):
     yq1 = y_mean @ q[1].T
     yq2 = y_mean @ q[2].T
     h = num_vox * yq1 @ yq1.T
-    e = sigma_sum +  num_vox * yq2 @ yq2.T
+    e = sigma_sum + num_vox * yq2 @ yq2.T
 
     return e, h
 
@@ -47,6 +49,52 @@ def get_size_yout_ymean(y):
     yout = np.einsum('bnr,anr->ba', y, y, optimize=True)
     ymean = y.mean(axis=2)
     return size, yout, ymean
+
+
+def get_rough(y, mask, mask_idx, n_shell=None):
+    """ computes roughness coefficient: tr_sigma / tr_sigma_other
+
+    where tr_sigma is the trace of sigma within the region and tr_sigma_other
+    is the trace of sigma for "other", see n_shell below
+
+    Args:
+        y (np.array): (b, num_img, num_vox) imaging features
+        mask (np.array): boolean mask, defines region
+        mask_idx (np.array): same shape as image.  -1 where voxel not
+            included in analysis, otherwise contains voxel index
+        n_shell (int): defines comparison "other" region.  if None then all
+            non-regions voxels are used, else a positive integer expected,
+            defines the width of the outer shell used in comparison volume
+    Returns:
+        rough (float): tr_sigma / tr_sigma_other
+    """
+    num_vox = y.shape[2]
+
+    def mask_to_bool(mask):
+        # produces boolean index corresponding to given mask
+        idx = mask_idx[mask]
+        bool_reg = np.zeros(num_vox, dtype=bool)
+        bool_reg[idx] = True
+        return bool_reg
+
+    # build bool_reg, boolean indexing for region
+    bool_reg = mask_to_bool(mask)
+
+    # build bool_other, boolean indexing for comparison volume
+    if n_shell is None:
+        bool_other = ~bool_reg
+    else:
+        mask_other = binary_dilation(mask, iterations=n_shell)
+        mask_other = mask_other & ~mask & (mask_idx > -1)
+        bool_other = mask_to_bool(mask_other)
+
+    assert bool_reg.sum() > 1, 'region contains 1 or fewer voxels'
+    assert bool_other.sum() > 1, 'other region contains 1 or fewer voxels'
+
+    sigma_reg = get_sigma_from_y(y[:, :, bool_reg])
+    sigma_other = get_sigma_from_y(y[:, :, bool_other])
+
+    return np.trace(sigma_reg) / np.trace(sigma_other)
 
 
 def get_sigma(size, yout, ymean):
