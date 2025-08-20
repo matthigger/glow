8affd661a3f2964b82fc0201142b57a45a347d73
matt higger <matt.higger@gmail.com>
2025-08-19 20:18:33 -0400

    deprecate - run.re_tailor() - no longer relevant, as written.  a similar function could work but it'd be preferable to just find the alpha_thresholds at which point the output changes, ROC curve style

--- hglm/compare_analyses/run.py
+++ hglm/compare_analyses/run.py
@@ -8,48 +8,11 @@ from uuid import uuid4
 
 import cloudpickle as pickle
 import numpy as np
-import pandas as pd
 
 import param
 from hglm.effect import ExtenterSphere, ExtenterMinVar
-from hglm.experiment import ExperimentScaled, tailor
-from hglm.graph import get_label_map
+from hglm.experiment import ExperimentScaled
 from hglm.mask import get_score
-
-
-def re_tailor(ana, alpha_tailor_all, mask_target, **kwargs):
-    """ re-runs tailor, returns dataframe of scores at every alpha_tailor
-
-    Args:
-        alpha_tailor_all (np.array): all alpha_tailor values to run
-        mask_target (mask): effect mask (target)
-
-    Returns:
-        df (pd.DataFrame):
-    """
-    rows = list()
-    children = ana.child_dict[0]
-    _pval_dict = ana.homo_pval_dict
-    for _alpha_tailor in alpha_tailor_all:
-        # re-tailor
-        reg_out_list, _pval_dict = tailor(sig_reg_list=ana.sig_reg_list,
-                                          alpha_tailor=_alpha_tailor,
-                                          exp=ana.exp,
-                                          children=children,
-                                          _pval_dict=_pval_dict,
-                                          **kwargs)
-
-        # compute scores
-        label_map = get_label_map(reg_out_list,
-                                  mask_idx=ana.exp.mask_idx,
-                                  children=children)
-        f1, sens, spec = get_score(mask_pred=label_map > -1,
-                                   mask_target=mask_target,
-                                   mask_active=ana.exp.mask_idx > -1)
-        rows.append(dict(alpha_tailor=_alpha_tailor, f1=f1, sens=sens,
-                         spec=spec))
-
-    return pd.DataFrame(rows)
 
 
 def run_one_exp(seed, hotel_tr):
