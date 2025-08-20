63273bef72e455e60eb1a1c732900e03a300b5d2
matt higger <matt.higger@gmail.com>
2025-08-19 20:17:31 -0400

    revise - tailor() - instead of bottom-up (or top-down) we discard the most heterogenous (and all ancestor) nodes until none are classified as heterogenous (i.e. each has pval > alpha_tailor). reg_out greedily selects the largest remaining regions such that the output is disjoint (merging all the homogenous effects)

--- hglm/experiment/tailor.py
+++ hglm/experiment/tailor.py
@@ -1,128 +1,150 @@
 import numpy as np
 
-import hglm.graph
 from .mancova import decompose, get_mancova
-from ..graph import get_node_desc_dict
+from ..graph import get_label_map, SCGraph, GRAPH_EXCLUDE
 
 
-def get_ll(y, **kwargs):
-    """ computes log likelihood of model """
-    e = get_mancova(y=y, **kwargs)[0]
-    s, ll = np.linalg.slogdet(e)
-    assert s != -1, 'error covariance not positive semi-definite'
+def get_llr(label_map, exp, _q_tup=None, _skip_homo=False):
+    """ gets log likelihood of homogenous vs heterogenous models
+
+    model0 (homo): all voxels follow same beta
+    model1 (hetero): each subset has its own beta
+
+    ll = log p(model0) / p(model1) = - det(e) + \sum_i det(e_i)
+
+    Args:
+        label_map (np.array): has same shape as exp.mask_idx, defines
+            partitioning of the whole region into subsets.  -1 outside
+            of analysis
+        exp (Experiment):
+        _q_tup (tup): see regress.decompose()
+        _skip_homo (bool): when False (default), subtracts -det(e).  otherwise
+            ignores this (its constant across perms in get_homo_pval,
+            setting to False avoids some computation)
+
+    Returns:
+        ll (float): log likelihood ratio
+    """
+
+    def _get_ll_one_reg(y, **kwargs):
+        """ computes log likelihood of model """
+        e = get_mancova(y=y, **kwargs)[0]
+        s, ll = np.linalg.slogdet(e)
+        assert s != -1, 'error covariance not positive semi-definite'
+
+        return ll
+
+    if _q_tup is None:
+        # decompose x into orthogonal spaces (full model)
+        contrast = np.zeros(exp.x.shape[0], dtype=bool)
+        _q_tup = decompose(exp.x, contrast)
+
+    # trim to relevant portion of y
+    mask = label_map > -1
+    idx = exp.mask_idx[mask]
+    y = exp.y[:, :, idx]
+
+    # compute the ll sum
+    if _skip_homo:
+        ll = -_get_ll_one_reg(y, q_tup=_q_tup)
+    else:
+        ll = 0
+
+    for label in np.unique(label_map):
+        if label == -1:
+            continue
+        b = (label_map == label)[mask]
+        ll += _get_ll_one_reg(y[:, :, b], q_tup=_q_tup)
 
     return ll
 
 
-def permute_llr_partition(x, y, partition, n_perm=200):
-    r""" llr under permutation test: n regions share same beta
-
-    model0: all voxels follow same beta
-    model1: each subset of partition has its own beta
-
-    log p(model0) / p(model1) = - det(e) + \sum_i det(e_i)
+def get_homo_pval(label_map, exp, n_perm):
+    """ run llr homogeneity test
 
     where e is the error covariance (see get_mancova()) of all voxels,
-    n is the total number of voxels and e_i and n_i are the values
-    for subset i
+    and e_i are corresponding values for subset i
 
     Args:
-        x (np.array): (a, num_img) explanatory variables
-        y (np.array): (b, num_img, num_vox) image intensities
-        partition (np.array): (num_vox) region index of each voxel in y
-            (agnostic to particular labels, its just the partitioning
-            of voxels into subsets that we need here)
+        label_map (np.array): has same shape as exp.mask_idx, defines
+            partitioning of the whole region into subsets.  -1 outside
+            of analysis
+        exp (Experiment):
         n_perm (int): number of permutations to perform (in addition
             to the unpermuted data)
 
     Returns:
-        llr (np.array): (n_perm + 1) log likelihood ratio.  llr[0] is
-            the llr of the original data
+        pval (float): percentage of permutations (including self) which are
+            more likely homogenous (low value suggests hetero)
     """
     # todo: count if too few permutations & warn as needed
 
-    # decompose x into orthogonal spaces
-    contrast = np.zeros(x.shape[0], dtype=bool)
-    q_tup = decompose(x, contrast)
+    # decompose x into orthogonal spaces (full model)
+    contrast = np.zeros(exp.x.shape[0], dtype=bool)
+    _q_tup = decompose(exp.x, contrast)
 
-    labels = np.unique(partition)
-    ll_whole = get_ll(y, q_tup=q_tup)
+    # compute llr
     rng = np.random.default_rng(seed=0)
-    llr_list = list()
+    llr = np.empty(n_perm + 1)
     for perm_idx in range(n_perm):
         if perm_idx:
             # perm_idx == 0 is original data
-            partition = rng.permutation(partition)
+            label_map = rng.permutation(label_map)
 
-        # compute the ll sum
-        term_sum = 0
-        for label in labels:
-            b = partition == label
-            term_sum += get_ll(y[:, :, b], q_tup=q_tup)
+        llr[perm_idx] = get_llr(label_map, exp, _q_tup=_q_tup, _skip_homo=True)
 
-        llr_list.append(term_sum - ll_whole)
-
-    return np.array(llr_list)
+    # compute p_value
+    return (llr[0] >= llr).mean()
 
 
-def tailor(sig_reg_list, children, exp, alpha_tailor, n_perm, _pval_dict=None):
+def tailor(sig_reg_list, children, exp, n_perm=300, alpha_tailor=.05):
     """ attempts to tailor to regions with all, and only, one effect
+
+    we discard the most heterogenous (and all ancestor) nodes until none are
+    classified as heterogenous (i.e. each has pval > alpha_tailor).
+    reg_out greedily selects the largest remaining regions such that the
+    output is disjoint (merging all the homogenous effects)
 
     Args:
         sig_reg_list (list): list of regions declared significant
         children (np.array): (num_reg, 2) each col are index of child
             regions
         exp (Experiment): the source data to run experiment on
-        alpha_tailor (float): threshold at which a prune event happens (a
-            significant region and all its ancestors are discarded)
-        n_perm (int): number of permutations to perform
+        n_perm (int): number of permutations to perform (homo test per region)
 
     Returns:
-        reg_out_list (list): index of prune regions
-        pval_homo_dict (dict): keys are region index, values are the pval
-            of that region being homogenous (small=hetero)
+        reg_out (np.array): index of output regions
     """
-    assert 1 / n_perm <= alpha_tailor, \
-        'inconsistent n_perm_tailor & alpha_tailor: all merge no prune'
+    # pre-compute: decompose x into orthogonal spaces (full model)
+    contrast = np.zeros(exp.x.shape[0], dtype=bool)
+    _q_tup = decompose(exp.x, contrast)
 
-    sig_reg_list = list(np.sort(sig_reg_list))
+    # get subgraph corresponding to significant regions
+    subgraph = SCGraph.from_children(children,
+                                     num_leaf=exp.y.shape[2],
+                                     subset=sig_reg_list)
 
-    def get_pval(parent, kid_list):
-        """ run homogeneity test (small pval = hetero) """
-        # build label map of partition of parent
-        label_map = hglm.graph.get_label_map(reg_idx_list=[parent] + kid_list,
-                                             mask_idx=exp.mask_idx,
-                                             children=children)
-
-        # permutation test: is the parent homogenous?
-        mask = label_map > -1
-        idx = exp.mask_idx[mask]
-        llr_list = permute_llr_partition(x=exp.x,
-                                         y=exp.y[:, :, idx],
-                                         partition=label_map[mask],
-                                         n_perm=n_perm)
-        return (llr_list[0] >= llr_list).mean()
-
-    # build parent representation of graph
-    sig_kid_dict = get_node_desc_dict(reg_idx_list=sig_reg_list,
-                                      children=children,
-                                      num_leaf=exp.y.shape[2])
-
-    # start with leaf nodes & apply all necessary merge operations
-    if _pval_dict is None:
-        _pval_dict = dict()
-    reg_out_set = set(sig_reg_list) - set(sig_kid_dict.keys())
-    for parent, kid_list in sorted(sig_kid_dict.items()):
-        if not reg_out_set.issuperset(kid_list):
-            # some child deemed heterogeneous, parent pruned as a result
+    # compute pval per region
+    homo_pval_dict = dict()
+    for par, kid_list in subgraph.children.items():
+        if not kid_list:
+            # "parent" is a leaf, no partitioning to test
             continue
+        label_map = get_label_map(reg_idx_list=[par] + kid_list,
+                                  mask_idx=exp.mask_idx,
+                                  children=children)
+        homo_pval_dict[par] = get_homo_pval(label_map, exp, n_perm=n_perm)
 
-        if parent not in _pval_dict:
-            _pval_dict[parent] = get_pval(parent, kid_list)
+    # sort homo_pval_dict small pvals to large
+    nodes_rm = set()
+    for reg_idx in sorted(homo_pval_dict.keys(), key=homo_pval_dict.get):
+        if homo_pval_dict[reg_idx] > alpha_tailor:
+            break
+        nodes_rm |= set(subgraph.iter_ancest(node=reg_idx, incl_self=True))
+    subgraph.modify(nodes_rm=nodes_rm)
 
-        if _pval_dict[parent] >= alpha_tailor:
-            # region is homogenous: merge (remove kids, add parent)
-            reg_out_set -= set(kid_list)
-            reg_out_set.add(parent)
+    # greedily choose disjoint & max size (i.e. in subgraph, parent not in subgraph)
+    no_parent = subgraph.parent == GRAPH_EXCLUDE
+    reg_out_list = np.where(subgraph.included & no_parent)[0]
 
-    return sorted(reg_out_set), _pval_dict
+    return reg_out_list, homo_pval_dict
--- test/experiment/test_tailor.py
+++ test/experiment/test_tailor.py
@@ -19,10 +19,10 @@ def test_tailor():
     # ensure region 0, 1, 2, 3 have sufficiently different stats
     exp.y[:, :, :4] += 100
 
-    reg_out_list, homo_pval_dict = tailor(sig_reg_list=sig_reg_list,
-                                          children=children,
-                                          exp=exp,
-                                          alpha_tailor=.05,
-                                          n_perm=100)
+    reg_out, homo_pval_dict = tailor(sig_reg_list=sig_reg_list,
+                                     children=children,
+                                     exp=exp,
+                                     alpha_tailor=.05,
+                                     n_perm=100)
 
-    assert reg_out_list == [10, 11, 12]
+    assert np.allclose(reg_out, [10, 11, 12])
